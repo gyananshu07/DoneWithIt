beda43b6a6a286d669a5c1b9b156c75221c94e01
gyananshu07 <2019041054@mmmut.ac.in>
2022-02-27 16:42:20 +0530

    Changes

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TouchableWithoutFeedback } from "react-native";
 
 export default function App() {
+  let x = 1;
+  console.log("Exe");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text>Hi!</Text>
+      <TouchableWithoutFeedback onPress={() => console.log("Pressed Image")}>
+        <Image
+          source={{
+            width: 300,
+            height: 300,
+            uri: "https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg",
+          }}
+        />
+      </TouchableWithoutFeedback>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
